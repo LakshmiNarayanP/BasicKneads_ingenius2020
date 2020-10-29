af404819ff2cd8c5114392b99400b22afc1c9447
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-30 00:11:30 +0530

    Welcome Page Completed

--- basickneads/lib/constants.dart
+++ basickneads/lib/constants.dart
@@ -7,3 +7,29 @@ BoxDecoration kDoodle = BoxDecoration(
     fit: BoxFit.cover,
   ),
 );
+
+class WPRaisedButton extends StatelessWidget {
+  const WPRaisedButton({this.buttonTitle});
+
+  final String buttonTitle;
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      minWidth: 300.0,
+      child: RaisedButton(
+        child: Text(
+          buttonTitle,
+          style: TextStyle(fontFamily: 'Poppins', fontSize: 25),
+        ),
+        onPressed: () => {print('pressed')},
+        color: Colors.grey[800],
+        textColor: Colors.white,
+        padding: EdgeInsets.all(8.0),
+        splashColor: Colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+      ),
+    );
+  }
+}
--- basickneads/lib/welcomePage.dart
+++ basickneads/lib/welcomePage.dart
@@ -35,35 +35,9 @@ class _WelcomePageState extends State<WelcomePage> {
             ),
             SizedBox(height: 30.0),
             WPRaisedButton(buttonTitle: 'Login'),
-            SizedBox(height: 40.0),
+            SizedBox(height: 30.0),
             WPRaisedButton(buttonTitle: 'Sign Up'),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class WPRaisedButton extends StatelessWidget {
-  const WPRaisedButton({this.buttonTitle});
-
-  final String buttonTitle;
-  @override
-  Widget build(BuildContext context) {
-    return ButtonTheme(
-      minWidth: 300.0,
-      child: RaisedButton(
-        child: Text(
-          buttonTitle,
-          style: TextStyle(fontFamily: 'Poppins', fontSize: 25),
-        ),
-        onPressed: () => {print('pressed')},
-        color: Colors.grey[850],
-        textColor: Colors.white,
-        padding: EdgeInsets.all(8.0),
-        splashColor: Colors.grey,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25.0),
         ),
       ),
     );
