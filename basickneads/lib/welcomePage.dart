99f87268dcc7a45f116611bb49b1a4f74e00c919
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-29 20:58:53 +0530

    m

--- basickneads/lib/welcomePage.dart
+++ basickneads/lib/welcomePage.dart
@@ -12,7 +12,7 @@ class _WelcomePageState extends State<WelcomePage> {
     return Container(
       decoration: kDoodle,
       child: Card(
-        color: Colors.blue,
+        color: Color(0xFFB800),
         margin: EdgeInsets.symmetric(vertical: 100.0, horizontal: 10),
         //child: Padding(
         //padding: EdgeInsets.all(80.0),
