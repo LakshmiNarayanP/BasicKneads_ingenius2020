1443819cc6b25bb8046e4aab3b390b0587930bbe
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-30 10:10:15 +0530

    Added Username and password fields

--- basickneads/lib/loginPage.dart
+++ basickneads/lib/loginPage.dart
@@ -21,10 +21,51 @@ class _LoginPageState extends State<LoginPage> {
               Text(
                 'Login',
                 style: kPoppinTitle,
-              )
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              EnterField(fieldTitle: 'Username:'),
+              SizedBox(height: 20.0),
+              EnterField(fieldTitle: 'Password:'),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class EnterField extends StatelessWidget {
+  EnterField({this.fieldTitle});
+
+  final String fieldTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                fieldTitle,
+                style: kPoppinTitle.copyWith(fontSize: 27.0),
+              ),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  borderSide: BorderSide(color: Colors.white, width: 1)),
+            ),
+          ),
+        ],
       ),
     );
   }
