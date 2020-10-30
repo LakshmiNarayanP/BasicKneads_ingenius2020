451c4a26b5785029f9a47f4574e11a4a08bb28be
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-30 17:06:43 +0530

    Spinner dependency added

--- basickneads/lib/startingpages/loginPage.dart
+++ basickneads/lib/startingpages/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:basickneads/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
