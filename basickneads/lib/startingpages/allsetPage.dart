8a79475e83ae918ea48a00eb50df1c6c4e6081f2
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-30 14:01:21 +0530

    All set page complete

--- basickneads/lib/startingpages/allsetPage.dart
+++ basickneads/lib/startingpages/allsetPage.dart
@@ -6,21 +6,8 @@ class AllSetPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: kDoodle,
-      child: Card(
-        color: Colors.grey[400],
-        margin: EdgeInsets.symmetric(vertical: 120.0, horizontal: 10),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20.0),
-          ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
-        ),
+      child: Image(
+        image: AssetImage('images/Group 84.png'),
       ),
     );
   }
