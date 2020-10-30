ee8c600aa8a7fc98cadb135172c1ccb654026469
SahanyaHebbar <sahanya.hebbar@hotmail.com>
2020-10-30 10:50:59 +0530

    Refactored the Login Page

--- basickneads/lib/constants.dart
+++ basickneads/lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 TextStyle kPoppinTitle = TextStyle(fontFamily: 'Poppins', fontSize: 40);
 BoxDecoration kDoodle = BoxDecoration(
@@ -58,10 +59,88 @@ class WPRaisedButton extends StatelessWidget {
         textColor: Colors.white,
         padding: EdgeInsets.all(8.0),
         splashColor: Colors.grey,
+        elevation: 10.0,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(25.0),
         ),
       ),
+    );
+  }
+}
+
+class EnterField extends StatelessWidget {
+  EnterField({this.fieldTitle});
+
+  final String fieldTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                fieldTitle,
+                style: kPoppinTitle.copyWith(fontSize: 27.0),
+              ),
+            ),
+          ),
+          Material(
+            elevation: 10.0,
+            shadowColor: Colors.grey[400],
+            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    borderSide: BorderSide(color: Colors.white, width: 1)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SocialButtons extends StatelessWidget {
+  const SocialButtons({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Or continue with',
+          style: kPoppinTitle.copyWith(fontSize: 18.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            ),
+            SignInButton(
+              Buttons.Facebook,
+              mini: true,
+              onPressed: () {},
+            ),
+            SignInButton(
+              Buttons.Twitter,
+              mini: true,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- basickneads/lib/loginPage.dart
+++ basickneads/lib/loginPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -35,63 +34,10 @@ class _LoginPageState extends State<LoginPage> {
                 onPressed: () => print('button pressed'),
               ),
               SizedBox(height: 25.0),
-              Text(
-                'Or continue with',
-                style: kPoppinTitle.copyWith(fontSize: 18.0),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SignInButton(
-                    Buttons.Facebook,
-                    mini: true,
-                    onPressed: () {},
-                  ),
-                ],
-              )
+              SocialButtons(),
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class EnterField extends StatelessWidget {
-  EnterField({this.fieldTitle});
-
-  final String fieldTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 30.0, right: 30.0),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                fieldTitle,
-                style: kPoppinTitle.copyWith(fontSize: 27.0),
-              ),
-            ),
-          ),
-          Material(
-            elevation: 10.0,
-            shadowColor: Colors.grey[400],
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    borderSide: BorderSide(color: Colors.white, width: 1)),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
